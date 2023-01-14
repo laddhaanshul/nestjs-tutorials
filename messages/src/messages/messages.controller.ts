ef37de7f60dc10d9bdf600b9ffb253eb2a4aa4ac
Anshul Laddha <laddhaanshul@gmail.com>
2023-01-15 00:37:51 +0530

    feat: :sparkles: Accessing request data with decorators like param, body, query, header

--- messages/src/messages/messages.controller.ts
+++ messages/src/messages/messages.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 
 @Controller('messages')
 export class MessagesController {
@@ -8,8 +8,12 @@ export class MessagesController {
   }
 
   @Post()
-  createMessages() {}
+  createMessages(@Body() body: any) {
+    console.log(body);
+  }
 
   @Get('/:id')
-  getMessages() {}
+  getMessages(@Param('id') id: string) {
+    console.log(id);
+  }
 }

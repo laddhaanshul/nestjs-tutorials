5c09fdc375ac27cf4e9a6e94272d7b8b7a93a2e6
Anshul Laddha <laddhaanshul@gmail.com>
2023-01-15 14:47:08 +0530

    feat: :safety_vest: Using pipes to validate request data

--- messages/src/messages/messages.controller.ts
+++ messages/src/messages/messages.controller.ts
@@ -1,14 +1,15 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
 
 @Controller('messages')
 export class MessagesController {
   @Get()
   listMessages() {
-    return 'Hello';
+    return '<h1>Hello</h1>';
   }
 
   @Post()
-  createMessages(@Body() body: any) {
+  createMessages(@Body() body: CreateMessageDto) {
     console.log(body);
   }
 
